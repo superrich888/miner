7aa9507eee3c4abe12a1f13417e94191c9a586ae
superrich888 <33969553+superrich888@users.noreply.github.com>
2021-05-16 22:22:34 +0700

    Create binance.sh

---
+++ binance.sh
@@ -0,0 +1,5 @@
+#binancecoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BNB:bnb1udq3s2yn2q7mq5mlpkrj98uj58s7dujmjrj7nw:560784.bless1+50000#8e6g-phqv --threads=4
